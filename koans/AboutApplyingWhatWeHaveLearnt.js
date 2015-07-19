ae43fbc3fd295c74b17e18d9c45df91d20f32eb2
Tobias Ammann <tag@adnm.ch>
2015-07-19 20:59:30 +0000

    Completed all Koans plus some bonus Koans

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -32,16 +32,16 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
 
-      var productsICanEat = [];
+      var productsICanEat = _(products)
+        .filter(function (pizza) { return !pizza.containsNuts; })
+        .filter(function (pizza) { return _(pizza.ingredients).all(function (ingredient) { return ingredient != 'mushrooms'; }) })
 
-      /* solve using filter() & all() / any() */
-
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
@@ -55,14 +55,16 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
 
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    var sum = _.range(1000)
+        .filter(function (n) { return n % 3 == 0 || n % 5 == 0; })
+        .reduce(function (agg, x) { return agg + x; }, 0)    /* try chaining range() and reduce() */
 
-    expect(233168).toBe(FILL_ME_IN);
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
@@ -75,39 +77,82 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   it("should count the ingredient occurrence (functional)", function () {
-    var ingredientCount = { "{ingredient name}": 0 };
+    var allIngredients = _.flatten(_(products).map(function (p) { return p.ingredients; }));
 
-    /* chain() together map(), flatten() and reduce() */
+    var ingredientCount = allIngredients.reduce(function (agg, x) {
+        agg[x] = (agg[x] || 0) + 1;
+        return agg;
+    }, {});
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   /*********************************************************************************/
   /* UNCOMMENT FOR EXTRA CREDIT */
   /*
   it("should find the largest prime factor of a composite number", function () {
-
+    
   });
 
   it("should find the largest palindrome made from the product of two 3 digit numbers", function () {
 
   });
-
-  it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
-
-
+  */
+  it("should find the smallest number divisible by each of the numbers 1 to 10", function () {
+    var ns = _.range(2,11);//21); // 2..10
+    
+    function found(x) {
+        return _.all(ns, function (n) {
+            return x % n == 0;
+        });
+    }
+    
+    var i = 1;
+    while (!found(i)) {
+        ++i;
+    }
+    
+    expect(i).toBe(2520);
   });
-
+  /*
   it("should find the difference between the sum of the squares and the square of the sums", function () {
 
   });
-
-  it("should find the 10001st prime", function () {
-
-  });
   */
+  it("should find the first 6 primes", function () {
+    function notN(n) {
+        return function (x) { return x % n !== 0; };
+    }
+    
+    var primes = []
+    var numbers = _.range(2, 20);
+    
+    for (var i = 0; i < 6; i++) {
+        primes.push(numbers[0]);
+        numbers = numbers.filter(notN(numbers[0]));
+    }
+    
+    expect(primes).toEqual([2,3,5,7,11,13]);
+  });
+
+
+  it("should find the 10001st prime", function () {/*
+    // commented out because it's a resource hog
+    function notN(n) {
+        return function (x) { return x % n !== 0; };
+    }
+    
+    var numbers = _.range(2, 110000);
+    
+    for (var i = 0; i < 10000; i++) {
+        numbers = numbers.filter(notN(numbers[0]));
+    }
+    
+    expect(numbers[0]).toBe(104743);*/
+  });
+  
 });
